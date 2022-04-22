89e6258e9afd924efe4a66820e9b767286f4ae81
marco <devapp.vs@outlook.com>
2022-04-22 23:18:37 +0200

    initial commit

---
+++ src/main/java/com/utility/keysFox/KeysFoxApplication.java
@@ -0,0 +1,13 @@
+package com.utility.keysFox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeysFoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeysFoxApplication.class, args);
+	}
+
+}
